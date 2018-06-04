f1b340ed6708528a1d985b0f7828e85e02f5ec9f
Tim Bakkum <timbakkum@gmail.com>
2018-06-04 14:35:35 +0200

    adds logging middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 import rootReducer from './../reducers';
 import mySaga from './../sagas';
 
@@ -9,12 +10,14 @@ import { routerMiddleware } from 'react-router-redux';
 const history = createHistory();
 const routingMiddleware = routerMiddleware(history);
 const sagaMiddleware = createSagaMiddleware();
+const loggerMiddleware = createLogger();
 
 // Only add certain middlewares in dev
 const middlewares =
   process.env.NODE_ENV !== 'production'
     ? [
         require('redux-immutable-state-invariant').default(),
+        loggerMiddleware,
         routingMiddleware,
         sagaMiddleware
       ]
